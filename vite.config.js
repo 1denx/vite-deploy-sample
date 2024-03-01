6b6b7003c2cf2b180de5260fb1af78483275f0fa
Eden <1denchen.work@gmail.com>
2024-03-02 00:41:09 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "vite-deploy-sample",
+  base: "/vite-deploy-sample/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
